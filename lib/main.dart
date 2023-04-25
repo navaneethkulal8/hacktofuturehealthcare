ff893ac11434e9892cc7d4d93c62680f08a73e2f
Navaneeth <navaneethkulal8@gmail.com>
2023-04-25 22:07:06 +0530

    medication sucessfull

--- lib/main.dart
+++ lib/main.dart
@@ -151,33 +151,141 @@ class NotificationsPage extends StatefulWidget {
 }
 
 class _NotificationsPageState extends State<NotificationsPage> {
-  List<String> _notifications = [];
-
-  void _addNotification() {
-    setState(() {
-      int notificationNumber = _notifications.length + 1;
-      _notifications.add('Notification $notificationNumber');
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Notifications Page'),
+        title: Text('My Medications'),
+        backgroundColor: Colors.green,
       ),
-      body: ListView.builder(
-        itemCount: _notifications.length,
-        itemBuilder: (context, index) {
-          return ListTile(
-            title: Text(_notifications[index]),
-          );
-        },
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _addNotification,
-        tooltip: 'Add Notification',
-        child: Icon(Icons.add),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(height: 20),
+              Text(
+                'Medications',
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 20),
+              TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Medication name',
+                ),
+              ),
+              SizedBox(height: 10),
+              TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Dose',
+                ),
+              ),
+              SizedBox(height: 10),
+              ElevatedButton.icon(
+                onPressed: () {},
+                label: Text('OK'),
+                icon: Icon(Icons.check),
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.green,
+                ),
+              ),
+              SizedBox(height: 20),
+              Card(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    ListTile(
+                      leading: Icon(Icons.album),
+                      title: Text('Medication Name'),
+                      subtitle: Text('Dose: 1 pill'),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 10),
+              Card(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    ListTile(
+                      leading: Icon(Icons.album),
+                      title: Text('Medication Name'),
+                      subtitle: Text('Dose: 1 pill'),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 10),
+              Card(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    ListTile(
+                      leading: Icon(Icons.album),
+                      title: Text('Medication Name'),
+                      subtitle: Text('Dose: 1 pill'),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 10),
+              Card(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    ListTile(
+                      leading: Icon(Icons.album),
+                      title: Text('Medication Name'),
+                      subtitle: Text('Dose: 1 pill'),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 10),
+              Card(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    ListTile(
+                      leading: Icon(Icons.album),
+                      title: Text('Medication Name'),
+                      subtitle: Text('Dose: 1 pill'),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 10),
+              Card(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    ListTile(
+                      leading: Icon(Icons.album),
+                      title: Text('Medication Name'),
+                      subtitle: Text('Dose: 1 pill'),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 10),
+              Card(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    ListTile(
+                      leading: Icon(Icons.album),
+                      title: Text('Medication Name'),
+                      subtitle: Text('Dose: 1 pill'),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
@@ -189,33 +297,140 @@ class ProfilePage extends StatefulWidget {
 }
 
 class _ProfilePageState extends State<ProfilePage> {
-  bool _isSwitched = false;
-
-  void _toggleSwitch(bool value) {
-    setState(() {
-      _isSwitched = value;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Profile Page'),
+        title: Text('My Medications'),
+        backgroundColor: Colors.green,
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Dark Mode',
-              style: Theme.of(context).textTheme.headline5,
-            ),
-            Switch(
-              value: _isSwitched,
-              onChanged: _toggleSwitch,
-            ),
-          ],
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(height: 20),
+              Text(
+                'Medications',
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 20),
+              TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Medication name',
+                ),
+              ),
+              SizedBox(height: 10),
+              TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Dose',
+                ),
+              ),
+              SizedBox(height: 10),
+              ElevatedButton.icon(
+                onPressed: () {},
+                label: Text('OK'),
+                icon: Icon(Icons.check),
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.green,
+                ),
+              ),
+              SizedBox(height: 20),
+              Card(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    ListTile(
+                      leading: Icon(Icons.album),
+                      title: Text('Medication Name'),
+                      subtitle: Text('Dose: 1 pill'),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 10),
+              Card(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    ListTile(
+                      leading: Icon(Icons.album),
+                      title: Text('Medication Name'),
+                      subtitle: Text('Dose: 1 pill'),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 10),
+              Card(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    ListTile(
+                      leading: Icon(Icons.album),
+                      title: Text('Medication Name'),
+                      subtitle: Text('Dose: 1 pill'),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 10),
+              Card(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    ListTile(
+                      leading: Icon(Icons.album),
+                      title: Text('Medication Name'),
+                      subtitle: Text('Dose: 1 pill'),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 10),
+              Card(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    ListTile(
+                      leading: Icon(Icons.album),
+                      title: Text('Medication Name'),
+                      subtitle: Text('Dose: 1 pill'),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 10),
+              Card(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    ListTile(
+                      leading: Icon(Icons.album),
+                      title: Text('Medication Name'),
+                      subtitle: Text('Dose: 1 pill'),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 10),
+              Card(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    ListTile(
+                      leading: Icon(Icons.album),
+                      title: Text('Medication Name'),
+                      subtitle: Text('Dose: 1 pill'),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
